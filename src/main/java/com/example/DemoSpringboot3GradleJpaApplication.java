f9c0c55e14f919eb2c4dcba19147d54fd15808a4
이세민 <blue92lee@hivelab.co.kr>
2024-01-11 14:08:49 +0900

    [수정] 최초 설정 작업

---
+++ src/main/java/com/example/DemoSpringboot3GradleJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringboot3GradleJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringboot3GradleJpaApplication.class, args);
+	}
+
+}
