8ce518585a0cc55d2e05d8bc97248b2f5324ef8a
이세민 <blue92lee@hivelab.co.kr>
2024-01-15 00:01:36 +0900

    [추가] p6spy 포매터 설정

---
+++ src/main/java/com/example/config/P6psyLogMessageConfig.java
@@ -0,0 +1,23 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import com.example.library.formatter.P6spySqlFormatter;
+import com.p6spy.engine.spy.P6SpyOptions;
+
+import jakarta.annotation.PostConstruct;
+
+/**
+ * P6spy 포맷 설정 클래스
+ *
+ * @author LEESEMIN
+ */
+// @Profile("local")
+@Configuration//(proxyBeanMethods = false)
+class P6psyLogMessageConfig {
+
+	@PostConstruct
+	public void setLogMessageFormat() {
+		P6SpyOptions.getActiveInstance().setLogMessageFormat(P6spySqlFormatter.class.getName());
+	}
+}
