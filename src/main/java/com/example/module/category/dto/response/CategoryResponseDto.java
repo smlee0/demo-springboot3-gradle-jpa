e3064f7edd51e78606dbf17c051b9851f72866f2
이세민 <blue92lee@hivelab.co.kr>
2024-01-17 14:44:18 +0900

    [수정] 누락 커밋

--- src/main/java/com/example/module/category/dto/response/CategoryResponseDto.java
+++ src/main/java/com/example/module/category/dto/response/CategoryResponseDto.java
@@ -1,11 +1,5 @@
 package com.example.module.category.dto.response;
 
-import java.util.List;
-
-import org.modelmapper.ModelMapper;
-import org.springframework.data.domain.Page;
-
-import com.example.entity.Category;
 import com.example.library.common.CommonFilter;
 
 import lombok.EqualsAndHashCode;
@@ -24,40 +18,8 @@ import lombok.ToString;
 @ToString(callSuper = true)
 public class CategoryResponseDto extends CommonFilter {
 
-	private static ModelMapper modelMapper = new ModelMapper();
-
 	private Long id;
 	private String name;
 	private String code;
 
-	/**
-	 * Entity -> DTO 변환
-	 *
-	 * @param category
-	 * @return
-	 */
-	public static CategoryResponseDto of(Category category) {
-		return modelMapper.map(category, CategoryResponseDto.class);
-	}
-
-	/**
-	 * Entity -> DTO 변환 (List의 경우)
-	 * @param categoryList
-	 * @return
-	 */
-	public static List<CategoryResponseDto> of(List<Category> categoryList) {
-		return categoryList.stream()
-			.map(CategoryResponseDto::of)
-			.toList();
-	}
-
-	/**
-	 * Entity -> DTO 변환 (Page의 경우)
-	 *
-	 * @param sourcePage
-	 * @return
-	 */
-	public static Page<CategoryResponseDto> of(Page<Category> sourcePage) {
-		return sourcePage.map(CategoryResponseDto::of);
-	}
 }
